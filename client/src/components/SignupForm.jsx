cbc630f8805e1fe71ac0a78acec3c6c562a6717a
EmmaDenton <edenton34@gmail.com>
2024-01-23 22:25:10 +1100

    Debugging

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { ADD_USER } from '../utils/mutation';
+import { ADD_USER } from '../utils/mutations';
 import { useMutation, gql } from '@apollo/client';
 import Auth from '../utils/auth';
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const SAVE_BOOK = gql`
-  mutation SaveBook($bookData: BookInput!) {
+  mutation saveBook($bookData: BookInput!) {
     saveBook(bookData: $bookData) {
       _id
       savedBooks {
@@ -16,8 +16,25 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-const ADD_USER = gql`
-  mutation AddUser($username: String!, $email: String!, $password: String!) {
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+  `;
+  
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
@@ -29,8 +46,8 @@ const ADD_USER = gql`
   }
 `;
 
-const LOGIN_USER = gql`
-  mutation Login($email: String!, $password: String!) {
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
     }
